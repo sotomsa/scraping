d4abf646ad8378261fae08e904ba3b0a49ef8080
Mauricio Soto <sotomsa@gmail.com>
2021-01-13 10:31:46 -0500

    Working copy of spider

--- scraping/scraping/spiders/elcolombinaospider.py
+++ scraping/scraping/spiders/elcolombinaospider.py
@@ -1,36 +1,11 @@
 import scrapy
-from scrapy.http import Response, FormRequest, Request
-from scrapy.utils.response import open_in_browser
 from ..items import ScrapingItem
-from selenium import webdriver
 
 class ColombianoSpider(scrapy.Spider):
     name = 'elcolombiano'
-    #start_urls = ["https://www.elcolombiano.com/antioquia#._531700976_p:1;"]
-    start_urls = ["https://www.elcolombiano.com/inicio-sesion"]
+    start_urls = ["https://www.elcolombiano.com/antioquia#._531700976_p:1;"]
 
-    def get_cookies(self):
-        driver = webdriver.Firefox()
-        driver.implicitly_wait(30)
-        base_url = "https://www.elcolombiano.com/inicio-sesion"
-        driver.get(base_url)
-        driver.find_element_by_name("userInput").clear()
-        driver.find_element_by_name("userInput").send_keys("sotomsa@gmail.com")
-        driver.find_element_by_name("inputPassword").clear()
-        driver.find_element_by_name("inputPassword").send_keys("71265msa")
-        driver.find_element_by_name("btnLogin").click()
-        cookies = driver.get_cookies()
-        driver.close()
-        return cookies
-
-    def parse(self, response, my_cookies=get_cookies):
-        return [Request(response, 
-                       url="https://www.elcolombiano.com/antioquia#._531700976_p:1;", 
-                       callback=self.parse_other,
-                       cookies=my_cookies)]
-
-    def parse_other(self, response):
-        open_in_browser(response)
+    def parse(self, response):
         articles = response.css(".paged  article")
         
         for article in articles:            
@@ -50,8 +25,7 @@ class ColombianoSpider(scrapy.Spider):
             items['date'] = date
             items['short_summary'] = short_summary
             items['url'] = url
-            yield scrapy.Request(url, callback=self.parse_news, cb_kwargs=items)
-            #yield items
+            yield items
 
     def parse_news(self, response, title, category, date, short_summary, url):
 
